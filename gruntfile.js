597048edb869c00b8db12fd9b5d8429c02766602
WildPastry <mykdsn@gmail.com>
2019-03-07 15:22:33 +1300

    Updated files

--- gruntfile.js
+++ gruntfile.js
@@ -61,6 +61,7 @@ module.exports = function (grunt) {
     });
 
     // LOAD TASKS
+    grunt.loadNpmTasks('grunt-browser-sync');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-watch');
