cfa127c20f73481d860aa9a56c6af517bfcae842
WildPastry <mykdsn@gmail.com>
2019-03-09 07:38:11 +1300

    WeatherWise file adjustments

--- README.md
+++ README.md
@@ -10,19 +10,21 @@ Weatherwise is a contemporary App which brings you more than the daily forecast.
 # GitHub team instrcutions
 • Clone the repository to your machine
 
-• Create a sass partial in /src/scss as: _yourname.scss
-
-• Create a script file in /src/js as: yourname.js
-
-• Edit main.scss to enable your partial
-
 • Navigate to the root directory of the repository in your terminal
 
 • Check node version is at least 10 (node -v)
 
 • Check npm version is at least 6 (node -v)
 
-• If your versions are up to date then: npm install
+• npm install
+
+• grunt (ALWAYS run 'grunt' command before doing any work)
+
+• Create a sass partial in /src/scss as: _yourname.scss
+
+• Create a script file in /src/js as: yourname.js
+
+• Edit main.scss to enable your partial
 
 • Create your local branch (git checkout -b yourname)
 
@@ -32,8 +34,6 @@ Weatherwise is a contemporary App which brings you more than the daily forecast.
 
 • NEVER work on or push to the master branch
 
-• In the terminal run: grunt
-
 • Once you have finished 'milestone' sections of code: first push to your remote branch so it's up to date, then push to the master-dev branch and resolve any conflicts
 
 • Every time you merge to the master-dev branch, let the other team members know. They MUST switch to the master-dev branch ASAP and pull (git checkout -b master-dev) + (git pull origin master-dev) Once this has been done you can switch back to your branch and continue working
@@ -41,7 +41,7 @@ Weatherwise is a contemporary App which brings you more than the daily forecast.
 • Merge to master-dev often to keep conflicts resolved
 
 # Design and Development
-WeatherWise is a collaborative student project designed and developed by Mike Parker, Lisa Simpson and Hannah Wehipeihana @ Yoobee School of Design.
+WeatherWise is a collaborative student project designed and developed by Mike Parker, Hannah Wehipeihana and Lisa Simpson @ Yoobee School of Design.
 
 # Copyrights
 Weather data and api service provided by Dark Sky API (https://darksky.net/dev)
@@ -49,3 +49,5 @@ Weather data and api service provided by Dark Sky API (https://darksky.net/dev)
 Some framework using Bootstrap (https://getbootstrap.com/)
 
 Some javascript functionality using jquery (http://jquery.com/)
+
+Graph API by Google Charts (https://developers.google.com/chart/)
---
+++ src/js/mike.js
@@ -0,0 +1,98 @@
+/*jslint browser:true */
+console.log('javascript ready...');
+
+$(document).ready(function () {
+    console.log("jquery ready...");
+});
+
+var skyKey;
+var dateStamp;
+var finalDateStamp;
+var months_arr = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+// SKY KEY
+$.ajax({
+    url: '../config.json',
+    dataType: 'json',
+    type: 'get',
+    success: function (keys) {
+        console.log('key loaded...');
+        skyKey = keys[0].SKY;
+        getSkyData();
+    },
+    error: function (error) {
+        console.log(error);
+        console.log('error getting key...');
+    }
+});
+
+// DATA
+function getSkyData() {
+    $.ajax({
+        url: 'https://api.darksky.net/forecast/' + skyKey + '/-41.2865,174.7762?units=si',
+        dataType: 'jsonp',
+        type: 'get',
+        success: function (skyData) {
+            console.log("SKY data loaded...");
+            console.log(skyData);
+            console.log("Current temperature:");
+            console.log(skyData.currently.temperature);
+            console.log(" ");
+
+            console.log("Daily data:");
+            for (var i = 0; i < skyData.daily.data.length; i++) {
+
+                // DATESTAMP
+                dateStamp = skyData.daily.data[i].time;
+
+                // DATESTAMP CONVERSION
+                var date = new Date(dateStamp * 1000);
+                var year = date.getFullYear();
+                var month = months_arr[date.getMonth()];
+                var day = date.getDate();
+
+                // if (day == 5) {
+                //     console.log('Tuesday');
+                // } //IF
+                // if (day == 6) {
+                //     console.log('Wednesday');
+                // } //IF
+                // if (day == 7) {
+                //     console.log('Thursday');
+                // } //IF
+                // if (day == 8) {
+                //     console.log('Friday');
+                // } //IF
+                // if (day == 9) {
+                //     console.log('Saturday');
+                // } //IF
+                // if (day == 10) {
+                //     console.log('Sunday');
+                // } //IF
+                // if (day == 11) {
+                //     console.log('Monday');
+                // } //IF
+                // if (day == 12) {
+                //     console.log('Tuesday');
+                // } //IF
+
+                var actualDate = month + '-' + day + '-' + year;
+                console.log(actualDate);
+                console.log(skyData.daily.data[i].summary);
+                console.log("Humidity:");
+                console.log(skyData.daily.data[i].humidity);
+                console.log("UV Index:");
+                console.log(skyData.daily.data[i].uvIndex);
+                console.log("Wind speed:");
+                console.log(skyData.daily.data[i].windSpeed);
+                console.log(" ");
+
+            }
+
+        }, //SUCCESS
+        error: function (error) {
+            console.log(error);
+            console.log('error getting data...');
+        }
+    }); //AJAX
+} //FUNCTION
