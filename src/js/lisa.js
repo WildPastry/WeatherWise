e0c85e9b5c35bce0659eaf4ae3320b75f2ab7257
Lisa Simpson <lisamaree02@Lisas-MacBook-Pro.local>
2019-03-17 15:13:05 +1300

    working on js

--- src/js/lisa.js
+++ src/js/lisa.js
@@ -33,10 +33,10 @@ var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
 
 // })
 
+//DATA
 
-
-
-jQuery.ajax({
+//jQuery.ajax({
+ ajax({
     url: 'https://api.darksky.net/forecast/9a5e19b54f8f0b91a70e71fec66307e9/42.3601,-71.0589',
     dataType: 'jsonp',
     type: "GET",
@@ -44,15 +44,24 @@ jQuery.ajax({
         console.log("Data loaded...");
         console.log(dataFromJSON);
 
-        //var sunrise = '';
-        jQuery.each(dataFromJSON.daily.data[0], function(i, val) {
-            // here you can do your magic
-            $("#data__time--sunrise").append(document.createTextNode(val.sunriseTime));
-            //$("#data__time--sunset").append(document.createTextNode(val.count));
-        });
+        var sunrise = [dataFromJSON.daily.data[0].sunriseTime];
+        var sunset = [dataFromJSON.daily.data[0].sunsetTime];
+        // var sunTimeData = [
+        //     [dataFromJSON.daily.data[0].sunriseTime],
+        //     [dataFromJSON.daily.data[0].sunsetTime]
+        //   ];
+        document.getElementById("#data__time--sunrise").innerHTML =  sunrise + " am";
+        //   if(document.getElementById("#data__time--sunrise") != null){
+        //     sunTimeData = document.getElementById("#data__time--sunrise").innerHTML;
+        // }
+        // var val = suntiseTime;
+        //  jQuery.each(dataFromJSON.daily.data[0], function(val) {
+        //         $("#data__time--sunrise").append(document.createTextNode(val.daily.data[0].sunriseTime));
+        //     //$("#data__time--sunset").append(document.createTextNode(val.count));
+        // });
 
   //for (var i = 0; i < data.length; i++) {
-        console.log(dataFromJSON.daily.data[0].sunriseTime);
+        //console.log(dataFromJSON.daily.data[0].sunriseTime);
         //console.log(dataFromJSON.daily.data[0].sunriseTime, dataFromJSON.daily.data[0].sunsetTime);
 
         //     title[i] = dataFromJSON.events[i].name.text;
@@ -61,9 +70,13 @@ jQuery.ajax({
         //     hour[i] = dataFromJSON.daily.data[0].sunriseTime.timezone;
         //     isFree[i] = dataFromJSON.daily.data[0].is_free;
 
-        //document.getElementById("#data__time--sunrise").innerHTML;
-        //         document.getElementById("#data__time--sunrise").innerHTML += title[i] + '&nbsp;' + hour[i] + '<br>';
+        //document.getElementById("#data__time--sunrise").innerHTML += daily.data[0].sunriseTime;
+          //document.getElementById("#data__time--sunrise").innerHTML += title[i] + '&nbsp;' + hour[i] + '<br>';
          //}
+
+
+
+
     },
 
     error: function (error) {
@@ -72,3 +85,25 @@ jQuery.ajax({
     }
 }); 
 
+// var data = google.visualization.arrayToDataTable([
+//           ['Age', 'Weight'],
+//           [demoData[0].age,demoData[0].weight],
+//           [demoData[1].age,demoData[1].weight],
+//           [demoData[2].age,demoData[2].weight],
+//           [demoData[3].age,demoData[3].weight],
+//           [demoData[4].age,demoData[4].weight],
+//           [demoData[5].age,demoData[5].weight]
+          
+//         ]);
+
+//         var options = {
+//           title: 'Age vs. Weight comparison',
+//           hAxis: {title: 'Age', minValue: 0, maxValue: 15},
+//           vAxis: {title: 'Weight', minValue: 0, maxValue: 15},
+//           legend: 'none'
+//         };
+
+//         var chart = new google.visualization.ScatterChart(document.getElementById('chart_div'));
+
+//         chart.draw(data, options);
+//       }
