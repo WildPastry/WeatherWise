5af8b074b3a0f4ed0dac1e7204b3f08dd7d1f0e6
Lisa Simpson <lisamaree02@Lisas-MacBook-Pro.local>
2019-03-17 22:11:10 +1300

    data is displayed

--- src/js/lisa.js
+++ src/js/lisa.js
@@ -1,51 +1,80 @@
-/*! weatherwise - v1.0.0 - 2019-03-17 */ 
-/* var sunriseTime = date;
+//  PLAN B SECONDS TO HOURS
+//  var time = sunrise
+// function secondsToHms(d) {
+//     d = dataFromJSON.daily.data[0].sunriseTime; //Number(d);
+//    console.log(hDisplay + mDisplay + sDisplay);
+//    var h = Math.floor(d / 3600);
+//    var m = Math.floor(d % 3600 / 60);
+//    var s = Math.floor(d % 3600 % 60);
+
+//    var hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " hours, ") : "";
+//    var mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " minutes, ") : "";
+//    var sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
+//    return hDisplay + mDisplay + sDisplay; 
+   //console.log(hDisplay + mDisplay + sDisplay); 
+//}
+
+/* var sunrise = date;
 //PLAN A SECONDS TO HOURS
  // Create a new JavaScript Date object based on the timestamp
 // multiplied by 1000 so that the argument is in milliseconds, not seconds.
 var date = new Date(unix_timestamp*1000);
 // Hours part from the timestamp
 var hours = date.getHours();
+console.log(date.getHours());
 // Minutes part from the timestamp
 var minutes = "0" + date.getMinutes();
 // Seconds part from the timestamp
 var seconds = "0" + date.getSeconds();
 
 // Will display time in 10:30:23 format
-var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2); */
-//  PLAN B SECONDS TO HOURS
-/*  var time = 
- function secondsToHms(d) {
-    d = dataFromJSON.daily.data[0].sunriseTime; //Number(d); 
-    //console.log(hDisplay + mDisplay + sDisplay);
-    var h = Math.floor(d / 3600);
-    var m = Math.floor(d % 3600 / 60);
-    //var s = Math.floor(d % 3600 % 60);
+var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);  */
 
-    var hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " hours, ") : "";
-    var mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " minutes, ") : "";
-    //var sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
-    return hDisplay + mDisplay + sDisplay; 
-    console.log(hDisplay + mDisplay + sDisplay); 
-}; */
 
-// $(document).ready(function(){
-
-// })
-
+// PAGE LOADER
+$('#master--loader').show();
+var getSunrise = document.getElementById("data__time--sunrise");
+var getSunset = document.getElementById("data__time--sunset");
 //DATA
-// console.log("hi");
+console.log("hi");
 //jQuery.ajax({
-function getDSData() {
+//function getDSData() {
  $.ajax({
     url: 'https://api.darksky.net/forecast/9a5e19b54f8f0b91a70e71fec66307e9/42.3601,-71.0589',
     dataType: 'jsonp',
     type: "GET",
     success: function (dataFromJSON) {
-        //console.log("Data loaded...");
-        //console.log(dataFromJSON);
+        console.log("Data loaded...");
+        console.log(dataFromJSON);
+
         sunrise = Math.trunc(dataFromJSON.daily.data[0].sunriseTime);
-        console.log(sunrise);
+        //getSunrise.innerHTML = '<h1 id="data__time--sunrise">' + sunrise + '</h1>';
+        getSunrise.innerHTML = '<p id="data__time--sunrise">' + sunrise + '</p>';
+
+        sunset = Math.trunc(dataFromJSON.daily.data[0].sunsetTime);
+        //getSunset.innerHTML = '<h1 id="data__time--sunset">' + sunset + ' ' + '</h1>';
+        getSunset.innerHTML = '<p id="data__time--sunset">' + sunset + ' ' + '</p>';
+
+
+
+       /*  function getSunRise() {
+            var sunrise = document.getElementById("data__time--sunrise");
+             getsunrise = Math.trunc(dataFromJSON.daily.data[0].sunriseTime);
+            //alert("function works");
+            return {
+                'srTime': sunrise,
+              };
+          }
+          window.onload = getSunRise; */
+        
+          
+        // function getSunRise(idS) {
+        //     var sunrise = document.getElementById(idS);
+        //     var sunrise = document.getElementById("data__time--sunrise");
+        // sunrise = Math.trunc(dataFromJSON.daily.data[0].sunriseTime);
+        // }
+        
+        // console.log(sunrise);
         //var sunrise = [dataFromJSON.daily.data[0].sunriseTime];
         //var sunset = [dataFromJSON.daily.data[0].sunsetTime];
         // var sunTimeData = [
@@ -86,7 +115,7 @@ function getDSData() {
     }
  }); 
  //ajax closed
-}
+//}
 //getDSData closed
 
 
@@ -122,13 +151,15 @@ function getTimeRemaining(endtime) {
       secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
   
       if (t.total <= 0) {
-        clearInterval(timeinterval);
+        clearInterval(timeinterval); //start counting down to the next 
       }
     }
   
     updateClock();
     var timeinterval = setInterval(updateClock, 1000);
   }
-  
+  //var deadline = new Date();
   var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
-  initializeClock('clockdiv', deadline);
+  initializeClock('countdown__div', deadline);
+
+  
--- src/js/main.js
+++ src/js/main.js
@@ -1,100 +1,81 @@
 /*! weatherwise - v1.0.0 - 2019-03-17 */ 
-/* $.ajax({
-    url: 'https://api.darksky.net/forecast/9a5e19b54f8f0b91a70e71fec66307e9/42.3601,-71.0589',
-    dataType: 'jsonp',
-    type: "GET",
-    success: function (dataFromJSON) {
-        //console.log("Data loaded...");
-        //console.log(dataFromJSON);
+//  PLAN B SECONDS TO HOURS
+//  var time = sunrise
+// function secondsToHms(d) {
+//     d = dataFromJSON.daily.data[0].sunriseTime; //Number(d);
+//    console.log(hDisplay + mDisplay + sDisplay);
+//    var h = Math.floor(d / 3600);
+//    var m = Math.floor(d % 3600 / 60);
+//    var s = Math.floor(d % 3600 % 60);
 
-        var sunrise = [dataFromJSON.daily.data[0].sunriseTime];
-        var sunset = [dataFromJSON.daily.data[0].sunsetTime];
-        // var sunTimeData = [
-        //     [dataFromJSON.daily.data[0].sunriseTime],
-        //     [dataFromJSON.daily.data[0].sunsetTime]
-        //   ];
-        document.getElementById("#data__time--sunrise").innerHTML =  sunrise + " am";
-        //   if(document.getElementById("#data__time--sunrise") != null){
-        //     sunTimeData = document.getElementById("#data__time--sunrise").innerHTML;
-        // }
-        // var val = suntiseTime;
-        //  jQuery.each(dataFromJSON.daily.data[0], function(val) {
-        //         $("#data__time--sunrise").append(document.createTextNode(val.daily.data[0].sunriseTime));
-        //     //$("#data__time--sunset").append(document.createTextNode(val.count));
-        // });
+//    var hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " hours, ") : "";
+//    var mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " minutes, ") : "";
+//    var sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
+//    return hDisplay + mDisplay + sDisplay; 
+   //console.log(hDisplay + mDisplay + sDisplay); 
+//}
 
-  //for (var i = 0; i < data.length; i++) {
-        //console.log(dataFromJSON.daily.data[0].sunriseTime);
-        //console.log(dataFromJSON.daily.data[0].sunriseTime, dataFromJSON.daily.data[0].sunsetTime);
-
-        //     title[i] = dataFromJSON.events[i].name.text;
-        console.log(dataFromJSON.daily.data[0].sunriseTime);
-        console.log(dataFromJSON.daily.data[0].sunsetTime);
-        //     hour[i] = dataFromJSON.daily.data[0].sunriseTime.timezone;
-        //     isFree[i] = dataFromJSON.daily.data[0].is_free;
-
-        //document.getElementById("#data__time--sunrise").innerHTML += daily.data[0].sunriseTime;
-          //document.getElementById("#data__time--sunrise").innerHTML += title[i] + '&nbsp;' + hour[i] + '<br>';
-         //}
-
-
-
-
-    },
-
-    error: function (error) {
-        console.log(error);
-        console.log("Error...");
-    }
-});  */
-/*! weatherwise - v1.0.0 - 2019-03-17 */ 
-/* var sunriseTime = date;
+/* var sunrise = date;
 //PLAN A SECONDS TO HOURS
  // Create a new JavaScript Date object based on the timestamp
 // multiplied by 1000 so that the argument is in milliseconds, not seconds.
 var date = new Date(unix_timestamp*1000);
 // Hours part from the timestamp
 var hours = date.getHours();
+console.log(date.getHours());
 // Minutes part from the timestamp
 var minutes = "0" + date.getMinutes();
 // Seconds part from the timestamp
 var seconds = "0" + date.getSeconds();
 
 // Will display time in 10:30:23 format
-var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2); */
-//  PLAN B SECONDS TO HOURS
-/*  var time = 
- function secondsToHms(d) {
-    d = dataFromJSON.daily.data[0].sunriseTime; //Number(d); 
-    //console.log(hDisplay + mDisplay + sDisplay);
-    var h = Math.floor(d / 3600);
-    var m = Math.floor(d % 3600 / 60);
-    //var s = Math.floor(d % 3600 % 60);
+var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);  */
 
-    var hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " hours, ") : "";
-    var mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " minutes, ") : "";
-    //var sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
-    return hDisplay + mDisplay + sDisplay; 
-    console.log(hDisplay + mDisplay + sDisplay); 
-}; */
 
-// $(document).ready(function(){
-
-// })
-
+// PAGE LOADER
+$('#master--loader').show();
+var getSunrise = document.getElementById("data__time--sunrise");
+var getSunset = document.getElementById("data__time--sunset");
 //DATA
-// console.log("hi");
+console.log("hi");
 //jQuery.ajax({
-function getDSData() {
+//function getDSData() {
  $.ajax({
     url: 'https://api.darksky.net/forecast/9a5e19b54f8f0b91a70e71fec66307e9/42.3601,-71.0589',
     dataType: 'jsonp',
     type: "GET",
     success: function (dataFromJSON) {
-        //console.log("Data loaded...");
-        //console.log(dataFromJSON);
+        console.log("Data loaded...");
+        console.log(dataFromJSON);
+
         sunrise = Math.trunc(dataFromJSON.daily.data[0].sunriseTime);
-        console.log(sunrise);
+        //getSunrise.innerHTML = '<h1 id="data__time--sunrise">' + sunrise + '</h1>';
+        getSunrise.innerHTML = '<p id="data__time--sunrise">' + sunrise + '</p>';
+
+        sunset = Math.trunc(dataFromJSON.daily.data[0].sunsetTime);
+        //getSunset.innerHTML = '<h1 id="data__time--sunset">' + sunset + ' ' + '</h1>';
+        getSunset.innerHTML = '<p id="data__time--sunset">' + sunset + ' ' + '</p>';
+
+
+
+       /*  function getSunRise() {
+            var sunrise = document.getElementById("data__time--sunrise");
+             getsunrise = Math.trunc(dataFromJSON.daily.data[0].sunriseTime);
+            //alert("function works");
+            return {
+                'srTime': sunrise,
+              };
+          }
+          window.onload = getSunRise; */
+        
+          
+        // function getSunRise(idS) {
+        //     var sunrise = document.getElementById(idS);
+        //     var sunrise = document.getElementById("data__time--sunrise");
+        // sunrise = Math.trunc(dataFromJSON.daily.data[0].sunriseTime);
+        // }
+        
+        // console.log(sunrise);
         //var sunrise = [dataFromJSON.daily.data[0].sunriseTime];
         //var sunset = [dataFromJSON.daily.data[0].sunsetTime];
         // var sunTimeData = [
@@ -135,7 +116,7 @@ function getDSData() {
     }
  }); 
  //ajax closed
-}
+//}
 //getDSData closed
 
 
@@ -171,13 +152,15 @@ function getTimeRemaining(endtime) {
       secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
   
       if (t.total <= 0) {
-        clearInterval(timeinterval);
+        clearInterval(timeinterval); //start counting down to the next 
       }
     }
   
     updateClock();
     var timeinterval = setInterval(updateClock, 1000);
   }
-  
+  //var deadline = new Date();
   var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
-  initializeClock('clockdiv', deadline);
+  initializeClock('countdown__div', deadline);
+
+  
