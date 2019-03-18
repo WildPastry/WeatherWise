17299596c2b4610ecf6b1434cde910f1f03bd02b
Hannah Wehipeihana <hannahwehipeihana@Hannahs-MacBook-Air.local>
2019-03-18 16:02:42 +1300

    Dynamic data and started working on the Area Chart

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,38 @@
-/*! weatherwise - v1.0.0 - 2019-03-14 */ 
+/*! weatherwise - v1.0.0 - 2019-03-18 */ 
+//LOADING THE PAGE
+$('#master--loader').show();
+var getHumidity = document.getElementById("data__humidity");
+var getUv = document.getElementById("data__uvIndex");
+var getWind = document.getElementById("data__windGust");
+var getTime = document.getElementById("data__time");
 
+//GETTING THE DATA
 $.ajax({
-  url: 'https://api.darksky.net/forecast/89c0665fe610a17c4d0e1f0b30da6e7a/-41.2865,174.7762',
+  url: 'https://api.darksky.net/forecast/89c0665fe610a17c4d0e1f0b30da6e7a/-41.28889,174.77722?units=si',
   dataType: 'jsonp',
-  type: "GET",
+  contentType: 'application/json',
+  type: 'GET',
   success: function (dataFromJSON) {
-      console.log(dataFromJSON);
+    console.log(dataFromJSON);
 
-      // for (var i = 0; i < dataFromJSON.events.length; i++) {
-      //   console.log(dataFromJSON.events[i].name, dataFromJSON.events[i].start);
+ //GETTING HUMIDITY
+    humidity = (dataFromJSON.daily.data[0].humidity)*100;
+    console.log(humidity);
+    getHumidity.innerHTML = '<h1>' + humidity  + '</h1>' + '<h4 class="inline">'+ '%' + '</h4>';
 
-      console.log(dataFromJSON.daily.data[0].humidity);
-        // title[i] = dataFromJSON.events[i].name.text;
-        // hour[i] = dataFromJSON.events[i].start.local;
-        // isFree[i] = dataFromJSON.events[i].is_free;
+//GETTING UV INDEX
+    uvIndex = (dataFromJSON.daily.data[0].uvIndex);
+    console.log(uvIndex);
+    getUv.innerHTML = '<h1>' + uvIndex + '</h1>';
 
-            // document.getElementById("info").innerHTML += title[i] + '&nbsp;' + hour[i] + '<br>';
-    // }
+ //GETTING WIND SPEED/GUST
+    windGust = Math.round((dataFromJSON.daily.data[0].windGust));
+    console.log(windGust);
+    getWind.innerHTML = '<h1>' + windGust + '</h1>' + '<h4 class="inline">'+ 'km/h' + '</h4>'; 
+
+//GETTING WIND SPEED/GUST
+  //  windGust = (dataFromJSON.daily.data[0].time);
+  //  console.log(time);
   },
 
   error: function (error) {
@@ -25,35 +41,51 @@ $.ajax({
   }
 });
 
+//AREA CHART FOR WIND SPEED DURING THE DAY
 google.charts.load('current', {'packages':['corechart']});
       google.charts.setOnLoadCallback(drawChart);
 
       function drawChart() {
-        var data = google.visualization.arrayToDataTable([
-          ['Time', 'Wind (km/h)'],
-          ['12:00',  50],
-          ['1:00',  20],
-          ['2:00',  30],
-          ['3:00',  80]
-        ]);
+          $.ajax({
+            url: 'https://api.darksky.net/forecast/89c0665fe610a17c4d0e1f0b30da6e7a/-41.28889,174.77722?units=si',
+            dataType: 'jsonp',
+            success: function(dataFromJSON) {
+              // for (var i=0; i <dataFromJSON.daily.data.length; i++) {
+  
+              // }
+            var data =new  google.visualization.DataTable();
+            data.addColumn ("number", "Wind");
+            data.addColumn ("number", "Days");
 
-        var options = {
-          indexLabel: 'Poppins',
-          titleTextStyle: 
-            {color: '#ffffff', fontSize: 10, fontName: 'Poppins'},
-          title: '',
-          legend: 'none',
-          hAxis: 
-            {textStyle:{color: '#ffffff', fontName: 'Poppins'}},
-          vAxis: 
-            {textStyle:{color: '#ffffff', fontName: 'Poppins'}},
-          backgroundColor: '#5a6977',
-          colors: ['#ffffff'],
-          tooltip: 
-            {textStyle: {color: '#5a6977', fontName: 'Poppins', fontSize: 10}
-          },
-        };
+            for (var i=0; i <dataFromJSON.daily.data.length; i++){
+              console.log(dataFromJSON.daily.data[i].windGust); 
+              data.addRow([dataFromJSON.daily.data[i].windGust, dataFromJSON.daily.data[i].time]);
+            }
+            var options = {
+              indexLabel: 'Poppins',
+              titleTextStyle: 
+                {color: '#ffffff', fontSize: 10, fontName: 'Poppins'},
+              title: '',
+              legend: 'none',
+              hAxis: 
+                {textStyle:{color: '#ffffff', fontName: 'Poppins'}},
+              vAxis: 
+                {textStyle:{color: '#ffffff', fontName: 'Poppins'}},
+              backgroundColor: '#5a6977',
+              colors: ['#ffffff'],
+              tooltip: 
+                {textStyle: {color: '#5a6977', fontName: 'Poppins', fontSize: 10}},
+            }; //OPTIONS ENDING
 
-        var chart = new google.visualization.AreaChart(document.getElementById('chart_div'));
+             var chart = new google.visualization.AreaChart(document.getElementById('chart_div'));
         chart.draw(data, options);
-      }
+            }, //SUCCESS ENDING
+
+        error:function(errorFromJSON){
+          console.log("Something has gone wrong");
+          alert("Error!");
+      } //ERROR ENDING
+    }); //AJAX ENDING
+ } //FUNCTION DRAW CHART ENDING
+      
+     
