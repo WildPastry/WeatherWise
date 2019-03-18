e5cf5cb32572818791bdadc8016028619c0e7347
WildPastry <mykdsn@gmail.com>
2019-03-18 17:22:31 +1300

    Updated master-dev merge

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 
-/*! weatherwise - v1.0.0 - 2019-03-14 */ /*! weatherwise - v1.0.0 - 2019-03-14 */ 
+/*! weatherwise - v1.0.0 - 2019-03-14 */ /*! weatherwise - v1.0.0 - 2019-03-14 */
 google.charts.load('current', {'packages':['corechart']});
 google.charts.setOnLoadCallback(drawChart);
 
@@ -14,17 +14,17 @@ function drawChart() {
 
   var options = {
 	indexLabel: 'Poppins',
-	titleTextStyle: 
+	titleTextStyle:
 	  {color: '#ffffff', fontSize: 10, fontName: 'Poppins'},
 	title: '',
 	legend: 'none',
-	hAxis: 
+	hAxis:
 	  {textStyle:{color: '#ffffff', fontName: 'Poppins'}},
-	vAxis: 
+	vAxis:
 	  {textStyle:{color: '#ffffff', fontName: 'Poppins'}},
 	backgroundColor: '#5a6977',
 	colors: ['#ffffff'],
-	tooltip: 
+	tooltip:
 	  {textStyle: {color: '#5a6977', fontName: 'Poppins', fontSize: 10}
 	},
   };
@@ -33,7 +33,7 @@ function drawChart() {
   chart.draw(data, options);
 }
 
-/*! weatherwise - v1.0.0 - 2019-03-18 */ 
+/*! weatherwise - v1.0.0 - 2019-03-18 */
 // CHECK JAVASCRIPT + JQUERY
 /*jslint browser:true */
 console.log('javascript ready...');
@@ -246,50 +246,6 @@ $.ajax({
 		  $("#dynamic > div:first-of-type").addClass("bckgd--db-v");
 		  $("#dynamic > div:first-of-type").removeClass("bckgd--y-m", "bckgd--b-m", "bckgd--db-m", "bckgd--g-m", "bckgd--g-v");
 	  }
-
-	  document.getElementById("data--daily--date").innerHTML = "";
-	  // document.getElementById("data--daily--summary").innerHTML = "";
-	  document.getElementById("data--daily--icon--text").innerHTML = "";
-	  document.getElementById("data--daily--temp").innerHTML = "";
-	  document.getElementById("data--daily--icon").innerHTML = "";
-
-	  console.log(" ");
-	  console.log("Daily data:");
-	  for (var i = 0; i < skyData.daily.data.length; i++) {
-
-		icon: 'clear-day.svg',
-	},
-	{
-		icon: 'clear-night.svg',
-	},
-	{
-		icon: 'rain.svg'
-	},
-	{
-		icon: 'snow.svg'
-	},
-	{
-		icon: 'sleet.svg'
-	},
-	{
-		icon: 'wind.svg'
-	},
-	{
-		icon: 'fog.svg'
-	},
-	{
-		icon: 'cloudy.svg'
-	},
-	{
-		icon: 'partly-cloudy-day.svg'
-	},
-	{
-		icon: 'partly-cloudy-night.svg'
-	},
-	{
-		icon: 'ww-location.svg'
-	}
-];
 
 // WRITE DEFAULT LOCATION TO APP
 getLocation.innerHTML = '<img class="icon--md" src="icon/' + dataIcons[10].icon + '">' + '<h2>' + "Wellington, New Zealand" + '</h2>';
@@ -517,10 +473,8 @@ function getSkyData() {
 			skyData.daily.data.shift();
 			for (var i = 0; i < skyData.daily.data.length; i++) {
 
-
 		  // DATESTAMP
 		  dateStamp = skyData.daily.data[i].time;
-
 
 		  // DATESTAMP CONVERSION
 		  var date = new Date(dateStamp * 1000);
@@ -727,7 +681,6 @@ function getSkyData() {
 
 	}); //AJAX
 
-
 } //FUNCTION
 
 // FORMAT DATE FUCNTIONS
@@ -810,7 +763,7 @@ document.getElementById("data--location").innerHTML = '<h2>' + input.value + '</
 
 console.log('Changed to London weather...');
 }
-/*! weatherwise - v1.0.0 - 2019-03-18 */ 
+/*! weatherwise - v1.0.0 - 2019-03-18 */
 //  PLAN B SECONDS TO HOURS
 //  var time = sunrise
 // function secondsToHms(d) {
@@ -823,12 +776,10 @@ console.log('Changed to London weather...');
 //    var hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " hours, ") : "";
 //    var mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " minutes, ") : "";
 //    var sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
-//    return hDisplay + mDisplay + sDisplay; 
-   //console.log(hDisplay + mDisplay + sDisplay); 
+//    return hDisplay + mDisplay + sDisplay;
+   //console.log(hDisplay + mDisplay + sDisplay);
 //}
 
-
- 
 // PAGE LOADER
 //$('#master--loader').show();
 // var getSunrise = document.getElementById("data__time--sunrise");
@@ -863,11 +814,11 @@ var lng = 174.7762;
         var minutes = "0" + date2.getMinutes();
         // Seconds part from the timestamp
         //var seconds = "0" + date2.getSeconds();
-        
+
         // Will display time in 10:30:23 format
         var formattedRiseTime = hours + ':' + minutes.substr(-2); // + ':' + seconds.substr(-2)
         console.log(formattedRiseTime);
-        
+
         sunset_timestamp = dataFromJSON.daily.data[0].sunsetTime;
 
         var date3 = new Date(sunset_timestamp*1000);
@@ -878,9 +829,9 @@ var lng = 174.7762;
         var minutesSet = "0" + date3.getMinutes();
         // Seconds part from the timestamp
         var secondsSet = "0" + date3.getSeconds();
-        
+
         // Will display time in 10:30:23 format
-        var formattedSunsetTime = hoursSet + ':' + minutesSet.substr(-2); 
+        var formattedSunsetTime = hoursSet + ':' + minutesSet.substr(-2);
         console.log(formattedSunsetTime);
 
         //DISPLAYING SUNRISE + SUNSET HTML
@@ -890,11 +841,10 @@ var lng = 174.7762;
          sunset = Math.trunc(dataFromJSON.daily.data[0].sunsetTime);
         // getSunset.innerHTML = '<div id="data__time--sunset">' + sunset + ' ' + '</div>';
 
-        
         getformattedRiseTime.innerHTML = '<p>' + formattedRiseTime + '</p>';
 
         getformattedSetTime.innerHTML = '<p>' +  formattedSunsetTime + '</p>';
-          
+
         // var timeString = formattedSunsetTime;
         // var H = +timeString.substr(0, 2);
         // var h = (H % 12) || 12;
@@ -902,19 +852,16 @@ var lng = 174.7762;
         // timeString = h + timeString.substr(2, 3) + ampm;
         // document.write(timeString);
 
-
-
     },
 
     error: function (error) {
         console.log(error);
         console.log("Error...");
     }
- }); 
+ });
  //ajax closed
 //}
 //getDSData closed
-
 
 //COUNTDOWN
 /* function getTimeRemaining(sunsetTime) {
@@ -931,27 +878,27 @@ var lng = 174.7762;
       'seconds': seconds
     };
   }
-  
+
   function initializeClock(id, sunsetTime) {
     var clock = document.getElementById(id);
     //var daysSpan = clock.querySelector('.days');
     var hoursSpan = clock.querySelector('.hours');
     var minutesSpan = clock.querySelector('.minutes');
     var secondsSpan = clock.querySelector('.seconds');
-  
+
     function updateClock() {
       var t = getTimeRemaining(sunsetTime);
-  
+
       //daysSpan.innerHTML = t.days;
       hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
       minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
       secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-  
+
       if (t.total <= 0) {
-        clearInterval(timeinterval); //start counting down to the next 
+        clearInterval(timeinterval); //start counting down to the next
       }
     }
-  
+
     updateClock();
     var timeinterval = setInterval(updateClock, 1000);
   }
@@ -967,23 +914,22 @@ var x = setInterval(function() {
 
   // Get todays date and time
   var now = new Date().getTime();
-    
+
   // Find the distance between now and the count down date
   var distance = countDownDate - now;
-    
+
   // Time calculations for days, hours, minutes and seconds
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
+
   // Output the result in an element with id="demo"
   document.getElementById("countdown__div").innerHTML = '<p>' + days + '</p>' + '<p>' + hours + '</p>' + '<p>' + minutes + '</p>';
-    
-  // If the count down is over, write some text 
+
+  // If the count down is over, write some text
   if (distance < 0) {
     clearInterval(x);
     document.getElementById("countdown__div").innerHTML = "EXPIRED";
   }
 }, 1000);
-
